b13012bba035f00fcbd90f44ad39c6c91b0e9099
mihaipaulorban <mihaipaulorban@gmail.com>
2024-02-02 15:20:01 +0100

    feat: aggiunta di comics con il metodo create

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,2 +1,19 @@
-<h1>{{ $comic->title }}</h1>
-<p>{{ $comic->description }}</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Includo gli assets con la direttiva @vite --}}
+    @vite ('resources/js/app.js')
+    <title>Comic Details</title>
+</head>
+<body class="d-flex vh-100 justify-content-center align-items-center">
+    <div class="container text-center">
+        <h1>{{ $comic->title }}</h1>
+        <p>{{ $comic->description }}</p>
+    </div>
+</body>
+
+</html>
